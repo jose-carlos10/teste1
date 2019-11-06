9f104422b7c218a5584b1878ec2f25cad710e71a
Coldzeca <jczeca15@gmail.com>
2019-11-06 15:36:45 -0300

    função que calcula distancia

---
+++ distancia.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>// necessário p/ as funções rand() e srand()
+#include <stdio.h>
+#include <time.h>//necessário p/ função time(
+
+//função que calcula a distancia
+float calculaDistancia( int x1, int y1, int x2, int y2) {
+return sqrt( pow x2-x1,2 + pow y2-y1,2 );
+}
+
+
+//main
+int main()
+{
+	
+}
