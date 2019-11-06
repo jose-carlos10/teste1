5bb3017bd12690be694ceff28441935f2c933cae
Coldzeca <jczeca15@gmail.com>
2019-11-06 16:48:15 -0300

    calcula distancia e mostra resultado

--- distancia.c
+++ distancia.c
@@ -6,7 +6,7 @@
 
 //função que calcula a distancia
 float calculaDistancia( int x1, int y1, int x2, int y2) {
-return sqrt( pow x2-x1,2 + pow y2-y1,2 );
+return sqrt( pow (x2-x1,2) + pow (y2-y1,2) );
 }
 
 
@@ -21,5 +21,13 @@ int main()
 	printf("Informe as cordenadas do primeiro ponto              ");
 	scanf("%i %i", &x1,&y1);
 	printf("Informe as cordenadas do segundo  ponto             ");
-	scanf("%i %i", &x2,&y2);	
+	scanf("%i %i", &x2,&y2);
+	
+	//calculo da distancia
+	distancia = calculaDistancia(x1,y1,x2,y2);	
+	
+	//mostra o rsultado
+	printf("Distancia : %f", distancia);
+	
+	return 0;
 }
